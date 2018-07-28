4e3e68f44f207ec0b9765375821533ed0afa7070
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 16:21:40 -0500

    added signup page and user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,16 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
