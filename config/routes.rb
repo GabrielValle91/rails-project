919dc2b721b8b51df05e35ab4ff9285a0ff09a03
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 12:57:11 -0500

    added nested routes and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
+  root 'application#homepage'
+
   resources :locations, only: [:index, :show, :new, :create, :edit, :update]
-  resources :drivers, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :drivers, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :shipments, only: [:index, :show, :new, :create, :edit, :update]
+  end
+
   resources :shipments, only: [:index, :show, :new, :create, :edit, :update]
+
   resources :items, only: [:index, :show, :new, :create, :edit, :update]
-  resources :clients, only: [:index, :show, :new, :create, :edit, :update]
-  resources :users, only: [:show, :new, :create]
+
+  resources :clients, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :shipments, only: [:index, :show, :new, :create, :edit, :update]
+    resources :items, only: [:index, :show, :new, :create, :edit, :update]
+  end
+  
+  resources :users, only: [:show, :new, :create] do
+    resources :clients, only: [:index, :show, :new, :create, :edit, :update]
+    resources :shipments, only: [:index, :show, :new, :create, :edit, :update]
+    resources :items, only: [:index, :show, :new, :create, :edit, :update]
+  end
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
