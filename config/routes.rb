cac6264a0fd4db1d3f37cd747316a4496bce8acc
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 12:45:55 -0500

    added index routes for several models

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :locations, only: [:show, :new, :create, :edit, :update]
-  resources :drivers, only: [:show, :new, :create, :edit, :update]
-  resources :shipments, only: [:show, :new, :create, :edit, :update]
-  resources :items, only: [:show, :new, :create, :edit, :update]
-  resources :clients, only: [:show, :new, :create, :edit, :update]
+  resources :locations, only: [:index, :show, :new, :create, :edit, :update]
+  resources :drivers, only: [:index, :show, :new, :create, :edit, :update]
+  resources :shipments, only: [:index, :show, :new, :create, :edit, :update]
+  resources :items, only: [:index, :show, :new, :create, :edit, :update]
+  resources :clients, only: [:index, :show, :new, :create, :edit, :update]
   resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
