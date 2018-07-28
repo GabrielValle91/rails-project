a53f80647c18744b3f1c7c901a34bdbc42fd56f2
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 12:50:12 -0500

    added sessions controller, methods and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show, :new, :create, :edit, :update]
   resources :clients, only: [:index, :show, :new, :create, :edit, :update]
   resources :users, only: [:show, :new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#logout'
 end
