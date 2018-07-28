737ad34e7cd469e9954811845c639b98361cf97b
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 12:42:04 -0500

    modified routes, added restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :locations
-  resources :drivers
-  resources :shipments
-  resources :items
-  resources :clients
-  resources :users
+  resources :locations, only: [:show, :new, :create, :edit, :update]
+  resources :drivers, only: [:show, :new, :create, :edit, :update]
+  resources :shipments, only: [:show, :new, :create, :edit, :update]
+  resources :items, only: [:show, :new, :create, :edit, :update]
+  resources :clients, only: [:show, :new, :create, :edit, :update]
+  resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
