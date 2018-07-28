d1a81e885e145243c775d9c8f10457a10efecdba
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 18:19:25 -0500

    added login page and controller action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,19 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
-
+    if session[:user_id]
+      redirect_to user_path(User.find(session[:id]))
+    else
+      @user = User.find_by(username: params[:username])
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to signin_url, notice: "incorrect password"
+      end
+    end
   end
 
   def logout
