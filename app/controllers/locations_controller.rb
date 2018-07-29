1f2d5a8e1c5976e20238dcaf8670a4419045648f
GabrielValle91 <gvalle91@gmail.com>
2018-07-29 15:27:48 -0500

    added helpers to location controller and before actions

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,8 @@
 class LocationsController < ApplicationController
+  before_action :user_auth
+  before_action :make_location, only: [:new]
+  before_action :find_location, only: [:edit, :update, :show]
+  
   def index
 
   end
@@ -21,5 +25,24 @@ class LocationsController < ApplicationController
 
   def update
 
+  end
+  private
+
+  def make_location
+    @location = Location.new
+  end
+
+  def find_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:item).permit(:company_name, :address1, :address2, :city, :state, :zip_code)
+  end
+
+  def user_auth
+    if !logged_in?
+      redirect_to signin_url
+    end
   end
 end
