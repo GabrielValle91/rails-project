60dd53dda989df4fdb47d138e2798f0460de1a80
GabrielValle91 <gvalle91@gmail.com>
2018-07-28 21:02:53 -0500

    fleshed out client create method, added flash message to client new form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -14,7 +14,16 @@ class ClientsController < ApplicationController
   end
 
   def create
-
+    @client = Client.new
+    @client.name = params[:client][:name]
+    @client.user_id = current_user.id
+    if @client.valid?
+      @client.save
+      redirect_to clients_path
+    else
+      flash[:notice] = "#{@client.errors.full_messages}"
+      redirect_to new_client_path
+    end
   end
 
   def edit
@@ -26,6 +35,10 @@ class ClientsController < ApplicationController
   end
 
   private
+
+  def client_params
+    params.require(:client).permit(:name, :user_id)
+  end
 
   def user_auth
     if !logged_in?
