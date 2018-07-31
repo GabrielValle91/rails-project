e6c544b4b394edc689fa217f0bf816af15106f40
GabrielValle91 <gvalle91@gmail.com>
2018-07-31 15:39:25 -0500

    reworked item controller to force nested routing, made error messages on item forms more readable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :user_auth
   before_action :make_item, only: [:new]
-  before_action :find_item, only: [:show, :edit, :update]
+  before_action :find_item, only: [:show]
 
   def index
     if params[:client_id]
@@ -12,42 +12,81 @@ class ItemsController < ApplicationController
   end
 
   def show
+    if !params[:client_id]
+      if correct_user
+        redirect_to user_client_item_path(current_user, @item.client, @item)
+      else
+        redirect_to user_items_path(current_user)
+      end
+    end
   end
 
   def new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to user_client_path(current_user, @item.client)
-    else
-      flash[:notice] = @item.errors.full_messages
-      if params[:item][:client_name]
-        redirect_to new_client_item_path(@item.client)
+    #raise params.inspect
+    if !params[:client_id]
+      if Client.find(params[:item][:client_id]).user == current_user
+        @item = Item.new(item_params)
+        if @item.save
+          redirect_to user_client_items_path(current_user, @item.client)
+        else
+          flash[:notice] = @item.errors.full_messages
+          redirect_to new_user_client_item_path(current_user, @item.client)
+        end
       else
-        redirect_to new_item_path(@item)
+        redirect_to new_user_item_path(current_user)
+      end
+    else
+      if current_client.user == current_user
+        @item = Item.new(item_params)
+        if @item.save
+          redirect_to user_client_items_path(current_user, current_client)
+        else
+          flash[:notice] = @item.errors.full_messages
+          redirect_to new_user_client_item_path(current_user, @item.client)
+        end
+      else
+        redirect_to new_user_item_path(current_user)
       end
     end
   end
 
   def edit
+    #raise params.inspect
+    if correct_user
+      if !params[:client_id]
+        redirect_to edit_client_item_path(@item.client, @item)
+      end
+    else
+      redirect_to user_items_path(current_user)
+    end
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to user_client_path(current_user, @item.client)
-    else
-      flash[:notice] = @item.errors.full_messages
-      if params[:item][:client_name]
-        redirect_to edit_client_item_path(@item.client)
+    if correct_user
+      if @item.update(item_params)
+        redirect_to user_client_path(current_user, @item.client)
       else
-        redirect_to edit_item_path(@item)
+        flash[:notice] = @item.errors.full_messages
+        redirect_to edit_client_item_path(@item.client)
       end
+    else
+      redirect_to user_items_path(current_user)
     end
   end
 
   private
+
+  def correct_user
+    find_item
+    if @item.client.user == current_user
+      return true
+    else
+      return false
+    end
+  end
 
   def make_item
     @item = Item.new
