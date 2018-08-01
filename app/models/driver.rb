78672da6e11d0a6232eb70968c35f46a8dd76762
GabrielValle91 <gvalle91@gmail.com>
2018-08-01 11:37:30 -0500

    added top 5 driver scope, added css to user show page

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -6,4 +6,6 @@ class Driver < ApplicationRecord
 
   LICENSES = ["Class A", "Class C"]
   DRIVERTYPES = ["Employee", "Owner Operator"]
+
+  scope :top_5, -> { joins(:shipment_details).group(:driver_id).order('COUNT(DISTINCT shipment_id) DESC').limit(5)}
 end
