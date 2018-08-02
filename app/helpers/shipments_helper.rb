5dd8bd4fe483a43f456451b9a2f1fef5166df9be
GabrielValle91 <gvalle91@gmail.com>
2018-08-02 16:37:07 -0500

    added shipment helper methods to format pickup and delivery dates

--- app/helpers/shipments_helper.rb
+++ app/helpers/shipments_helper.rb
@@ -1,2 +1,9 @@
 module ShipmentsHelper
+  def shipping_date(shipment)
+    shipment.pickup_date.strftime("%m/%d/%Y - %A")
+  end
+
+  def delivery_date(shipment)
+    shipment.delivery_date.strftime("%m/%d/%Y - %A")
+  end
 end
