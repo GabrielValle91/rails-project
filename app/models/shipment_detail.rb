a66c3398630b8a04ac48a1acd42a4bb2bdd156ac
GabrielValle91 <gvalle91@gmail.com>
2018-08-02 10:05:29 -0500

    implemented shipment show page. Created a couple scope methods to grab shipment detail information

--- app/models/shipment_detail.rb
+++ app/models/shipment_detail.rb
@@ -2,4 +2,12 @@ class ShipmentDetail < ApplicationRecord
   belongs_to :shipment
   belongs_to :driver, optional: true
   belongs_to :location, optional: true
+
+  scope :shipper, -> { joins(:location).where(location_type: "shipper").limit(1).first}
+
+  scope :pickup_driver, -> { joins(:driver).where(location_type: "shipper").limit(1).first}
+
+  scope :consignee, -> {joins(:location).where(location_type: "consignee").limit(1).first}
+
+  scope :delivery_driver, -> { joins(:driver).where(location_type: "consignee").limit(1).first}
 end
