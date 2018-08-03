ee5ad171074da2aff7a7cbe62c4a6862eac31f2f
GabrielValle91 <gvalle91@gmail.com>
2018-08-03 11:31:01 -0500

    login via github works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,29 @@ class SessionsController < ApplicationController
     if session[:user_id]
       redirect_to user_path(User.find(session[:id]))
     else
-      @user = User.find_by(username: params[:username])
-      if @user
-        if @user.authenticate(params[:password])
+      if auth_hash = request.env["omniauth.auth"]
+        user_name = auth_hash[:info][:name]
+        user_email = auth_hash[:info][:email]
+        if @user = User.find_by(email: user_email)
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          @user = User.new(username: user_name, email: user_email, password: SecureRandom.hex)
+          if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+          else
+            redirect_to signin_url, notice: "something went wrong, please try again"
+          end
+        end
+      else
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect_to user_path(@user)
         else
           redirect_to signin_url, notice: "incorrect username/password combination"
         end
-      else
-        redirect_to signin_url, notice: "incorrect username/password combination"
       end
     end
   end
