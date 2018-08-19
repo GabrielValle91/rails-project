b9df2c767cb0895dc258c58c6d23bdcadc264ff8
GabrielValle91 <gvalle91@gmail.com>
2018-08-19 14:28:19 -0500

    added location browsing with json

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -11,4 +11,27 @@ $(function() {
     let id = $(this).data("id");
     $("#location-details-" + id).empty();
   });
+
+  $(".next-location").on('click', function(){
+    let id = parseInt($("#location-id")["0"].innerHTML);
+    let max = $(this).data("max");
+    locationShow(id, max);
+  });
 });
+
+function locationShow (id, max){
+  id >= max ? id = 1 : id += 1;
+  $.get("/locations/" + id + ".json", function() {
+  })
+    .success(function(locationData){
+      $("#company-name").html(locationData["company_name"]);
+      $("#address-1").html(locationData["address1"]);
+      $("#address-2").html(locationData["address2"]);
+      let locationDetails = locationData["city"] + ", " + locationData["state"] + " " + locationData["zip_code"];
+      $("#address-city-state-zip").html(locationDetails);
+      $("#location-id").html(id);
+    })
+    .fail(function (){
+      locationShow(id);
+    });
+}
