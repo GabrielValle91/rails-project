a639477cf5e8a0f7253675f10843794c1ee98779
GabrielValle91 <gvalle91@gmail.com>
2018-08-19 20:44:34 -0500

    replaced unassigned shipments with JSON equivalent

---
+++ app/assets/javascripts/dispatch.js
@@ -0,0 +1,24 @@
+$(function (){
+  populateUnassignedList();
+})
+
+function getFormattedDate(date) {
+  var d = new Date(date);
+  var year = d.getFullYear();
+  var month = (1 + d.getMonth()).toString();
+  month = month.length > 1 ? month : '0' + month;
+  var day = d.getDate() + 1;
+  day = day.toString();
+  day = day.length > 1 ? day : '0' + day;
+  return month + '/' + day + '/' + year;
+}
+
+function populateUnassignedList(){
+  $("#unassigned-shipment-list").empty();
+  $.get("/dispatch/unassignedshipments", function (shipmentData){
+    shipmentData.forEach(function(shipment){
+      let newRow = "<tr><td>" + shipment.id + "</td><td>" + shipment.client.name + "</td><td>" + getFormattedDate(shipment.pickup_date) + "</td><td>" + getFormattedDate(shipment.delivery_date) + "</td></tr>"
+      $("#unassigned-shipment-list").append(newRow);
+    })
+  })
+}
